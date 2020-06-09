1cbe554be7b3a0ddc22eeb51b0b1fe5a5be96f7f
miyatayo0228 <beebees10c@gmail.com>
2020-06-09 18:01:54 +0900

    出品機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,17 @@
 class ItemsController < ApplicationController
-  def index
+
+  def new
+    @item = Item.new
   end
+
+  def create
+    Item.create(item_params)
+    redirect_to root_path
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:item_name)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "posts#index"
   resources :posts, only: [:index,:show]
-  resources :items, only: [:index]
+  resources :items, only: [:new,:create]
   resources :mypages, only: [:index]
   resources :login, only: [:index]
   resources :signup, only: [:index]
