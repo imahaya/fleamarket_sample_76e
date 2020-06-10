cff64cfe13180a56bf312134a0cbe8aef0278f68
sosososo24 <suzuki.s.1024@gmail.com>
2020-06-10 12:10:03 +0900

    intial commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,21 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:password])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name_kana])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name_kana])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:birthday])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:phone_number])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:encrypted_password])
+  end
 
   private
 
