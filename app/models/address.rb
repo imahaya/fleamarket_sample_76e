b2f32e07e3881623ec972bca1e7ba95b4064e5c6
sosososo24 <suzuki.s.1024@gmail.com>
2020-06-13 11:45:47 +0900

    修正４

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,16 +1,9 @@
 class Address < ApplicationRecord
-  # belongs_to :user
-  # belongs_to :seller
-  # belongs_to :buyer
 
-  validates :shopping_family_name, presence: true
-  validates :shopping_first_name, presence: true
-  validates :shopping_family_name_kana, presence: true
-  validates :shopping_first_name_kana, presence: true
-  validates :post_code, presence: true
-  validates :prefecture_code, presence: true
-  validates :city, presence: true
-  validates :address, presence: true
-  validates :buildingname, presence: true
-  validates :phone_number, presence: true
+  belongs_to :user
+  belongs_to :seller, optional: true
+  belongs_to :buyer, optional: true
+
+  validates :shopping_family_name, :shopping_first_name, :shopping_family_name_kana, :shopping_first_name_kana, :post_code, :prefecture_code, :city, :address, :buildingname, :phone_number,presence: true
+
 end
