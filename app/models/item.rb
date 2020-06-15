f63d41ee2cc6cec2f86bcdcf89fadb1b026335f6
sosososo24 <suzuki.s.1024@gmail.com>
2020-06-15 11:33:21 +0900

    ancestry 2 (seed.rb)

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,7 +10,12 @@ class Item < ApplicationRecord
 
   
   belongs_to :user,required: false
+  
   # belongs_to :category,required: false
+
+  # belongs_to user, foreign_key: 'user_id'
+
+  belongs_to :category
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
 
