658c480c54f163065ddde526f4a9fc0c763a653d
imahaya <hayato0524lovenmb48@icloud.com>
2020-06-15 19:35:53 +0900

    商品一覧表示機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,25 @@
 class PostsController < ApplicationController
 
   def index
+    @items = Item.all.order("created_at DESC").limit(3)
+  end
+
+  def new
+
+  end
+
+  def create
+
   end
 
   def show
+    @item = Item.find(params[:id])
+  end
+
+
+  def post_params
+    params.require(:images).permit(:imgge)
+    params.require(:items).permit(:item_name, :price)
   end
   
 end
