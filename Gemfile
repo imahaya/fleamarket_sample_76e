4772a63716eacc3abc17a07247a7243c0756b1f0
miyatayo0228 <beebees10c@gmail.com>
2020-06-15 11:00:08 +0900

    カード修正1

--- Gemfile
+++ Gemfile
@@ -84,4 +84,4 @@ gem 'fog-aws'
 gem 'devise-i18n'
 gem 'devise-i18n-views'
 gem 'pry-rails'
-
+gem 'payjp'
--- db/schema.rb
+++ db/schema.rb
@@ -46,7 +46,7 @@ ActiveRecord::Schema.define(version: 2020_06_09_014125) do
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
-    t.string "cardr_id", null: false
+    t.string "card_id", null: false
     t.string "customer_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
