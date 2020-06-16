c2f8affd466d1221470031fe0d25f2fdeb6938dc
takutakuminmin <takutakuminmin0730@icloud.com>
2020-06-16 21:19:07 +0900

    修正10

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,5 +1,5 @@
 class PurchasesController < ApplicationController
-
+  before_action :secret_key only: [:index, :pay]
   require 'payjp'
 
   def index
@@ -7,9 +7,9 @@ class PurchasesController < ApplicationController
     #Cardテーブルは前回記事で作成、テーブルからpayjpの顧客IDを検索
     if card.blank?
       #登録された情報がない場合にカード登録画面に移動
-      # redirect_to controller: "card", action: "new"
+      redirect_to controller: "cards", action: "new"
     else
-      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      # Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
       #保管した顧客IDでpayjpから情報取得
       customer = Payjp::Customer.retrieve(card.customer_id)
       #保管したカードIDでpayjpから情報取得、カード情報表示のためインスタンス変数に代入
@@ -18,8 +18,8 @@ class PurchasesController < ApplicationController
   end
 
   def pay
+
     card = Card.where(user_id: current_user.id).first
-    Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
     Payjp::Charge.create(
     :amount => 13500, #支払金額を入力（itemテーブル等に紐づけても良い）
     :customer => card.customer_id, #顧客ID
@@ -28,4 +28,9 @@ class PurchasesController < ApplicationController
   redirect_to action: 'done' #完了画面に移動
   end
 
+  private
+
+  def secret_key
+    Payjp.api_key = Rails.application.credentials.payjp[:secret_key]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,15 @@ Rails.application.routes.draw do
   resources :posts, only: [:index,:show]
   resources :items, only: [:new,:create]
   resources :mypages, only: [:index]
-  resources :purchases, only: [:index]
   resources :addresses, only: [:new, :create]
   resources :users, only: [:edit, :update]
   resources :cards, only: [:new, :show, :edit, :update, :create, :destroy]
+
+  resources :purchases, only: [:index] do
+    collection do
+      get 'index', to: 'purchases#index'
+      post 'pay', to: 'purchases#pay'
+      get 'done', to: 'purchases#done'
+    end
+  end
 end
