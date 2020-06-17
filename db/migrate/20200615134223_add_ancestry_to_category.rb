f99e83a15c56d43cea97e32aa2caf8a6aecbb253
MasahiroMatsuno <mmvv596@hotmail.co.jp>
2020-06-17 12:36:50 +0900

    マイグレーション修正

--- db/migrate/20200615134223_add_ancestry_to_category.rb
+++
@@ -1,3 +0,0 @@
-class AddAncestryToCategory < ActiveRecord::Migration[5.2]
-  
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_15_134223) do
+ActiveRecord::Schema.define(version: 2020_06_15_014559) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "shopping_family_name", null: false
