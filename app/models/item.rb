91d04a6323c2ac89fad8faba8df08d3ea584df34
MasahiroMatsuno <mmvv596@hotmail.co.jp>
2020-06-18 18:31:07 +0900

    カテゴリー機能編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to :user,required: false
   # belongs_to :category,required: false
+  belongs_to :category
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
 
