f27d252fb4f5f98264baa9493e27dad0f8cf68fc
MasahiroMatsuno <mmvv596@hotmail.co.jp>
2020-06-19 12:33:32 +0900

    修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,9 +15,10 @@ class Item < ApplicationRecord
 
  
 
-  validates :images, :item_name, :prefecture_id, :price, :day,:delivery_fee, :condition, :category_id, presence: true
+  validates :images, :item_name, :prefecture_id, :price, :day,:delivery_fee, :condition, :category_id,:child_category_id, presence: true
 
   validates :introduction, presence: true, length: { maximum: 1000 }
 
+  # validates :category, presence: true
 
 end
